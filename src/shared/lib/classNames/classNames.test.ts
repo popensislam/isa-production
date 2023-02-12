d45bc8a27ae73d842e4e8a174e24fe9e4f8126ed
popensisa <s.islam.like@mail.ru>
2023-02-12 21:25:30 +0600

    initial tests

--- src/shared/lib/classNames/classNames.test.ts
+++ src/shared/lib/classNames/classNames.test.ts
@@ -1,5 +1,21 @@
+import { classNames } from './classNames';
+
 describe('classNames', () => {
-  test('test', () => {
-    expect(true).toBe(true);
+  test('classNames with first params', () => {
+    expect(classNames('testClass')).toBe('testClass');
+  });
+  test('classNames with the first two params', () => {
+    expect(classNames('testClass', { hovered: true })).toBe('testClass hovered');
+  });
+  test('classNames with all params', () => {
+    expect(classNames('testClass', { hovered: true }, [ 'active' ])).toBe('testClass active hovered');
+  });
+  test('classNames with a lot of params', () => {
+    expect(classNames('testClass', { hovered: true, active: true, focus: false }, [ 'block', 'header' ]))
+      .toBe('testClass block header hovered active');
+  });
+  test('classNames with undefined', () => {
+    expect(classNames('testClass', { hovered: true, focus: undefined }))
+      .toBe('testClass hovered');
   });
 });
