1eb48d8c4c3eac76a2e94d97357d81f70839fc2c
popensisa <s.islam.like@mail.ru>
2023-02-22 16:19:02 +0600

    render with router

--- src/shared/ui/AppLink/AppLink.test.tsx
+++ src/shared/ui/AppLink/AppLink.test.tsx
@@ -1,21 +1,21 @@
 import { screen } from '@testing-library/react';
-import { renderWithRouter } from 'shared/lib/renderWithRouter/renderWithRouter';
+import { componentRender } from 'shared/lib/componentRender/componentRender';
 import { AppLink, AppLinkTheme } from './AppLink';
 
 
 describe('AppLink', () => {
   test('AppLink primary', () => {
-    renderWithRouter(<AppLink to='/'>AppLink</AppLink>);
+    componentRender(<AppLink to='/'>AppLink</AppLink>);
     expect(screen.getByText('AppLink')).toBeInTheDocument();
   });
 
   test('AppLink primary', () => {
-    renderWithRouter(<AppLink to='/' theme={AppLinkTheme.PRIMARY}>AppLink</AppLink>);
+    componentRender(<AppLink to='/' theme={AppLinkTheme.PRIMARY}>AppLink</AppLink>);
     expect(screen.getByText('AppLink')).toHaveClass('primary');
   });
 
   test('AppLink secondary', () => {
-    renderWithRouter(<AppLink to='/' theme={AppLinkTheme.SECONDARY}>AppLink</AppLink>);
+    componentRender(<AppLink to='/' theme={AppLinkTheme.SECONDARY}>AppLink</AppLink>);
     expect(screen.getByText('AppLink')).toHaveClass('secondary');
   });
 });
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,17 +1,16 @@
 /* eslint-disable i18next/no-literal-string */
 import { fireEvent, screen } from '@testing-library/react';
-import { renderWithRouter } from 'shared/lib/renderWithRouter/renderWithRouter';
-import { renderWithTranslation } from 'shared/lib/renderWithTranslation/renderWithTransltion';
+import { componentRender } from 'shared/lib/componentRender/componentRender';
 import { Sidebar } from 'widgets/Sidebar/ui/Sidebar/Sidebar';
 
 describe('Sidebar', () => {
   test('Sidebar render', () => {
-    renderWithTranslation(renderWithTranslation(<Sidebar/>));
+    componentRender(<Sidebar/>);
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
   });
 
   test('Sidebar toggle', () => {
-    renderWithRouter(renderWithTranslation(<Sidebar/>));
+    componentRender(<Sidebar/>);
     const toggleBtn = screen.getByTestId('sidebar-toggle');
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     fireEvent.click(toggleBtn);
