2ffb0692812b5e5cf46a93ab800bf224bee207e6
popensisa <s.islam.like@mail.ru>
2023-02-22 15:54:39 +0600

    render with route

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,16 +1,17 @@
 /* eslint-disable i18next/no-literal-string */
 import { fireEvent, screen } from '@testing-library/react';
+import { renderWithRouter } from 'shared/lib/renderWithRouter/renderWithRouter';
 import { renderWithTranslation } from 'shared/lib/renderWithTranslation/renderWithTransltion';
 import { Sidebar } from 'widgets/Sidebar/ui/Sidebar/Sidebar';
 
 describe('Sidebar', () => {
   test('Sidebar render', () => {
-    renderWithTranslation(<Sidebar/>);
+    renderWithTranslation(renderWithTranslation(<Sidebar/>));
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
   });
 
   test('Sidebar toggle', () => {
-    renderWithTranslation(<Sidebar/>);
+    renderWithRouter(renderWithTranslation(<Sidebar/>));
     const toggleBtn = screen.getByTestId('sidebar-toggle');
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     fireEvent.click(toggleBtn);
