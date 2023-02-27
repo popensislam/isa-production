c7e4543ad9b898bdc1b655c5cb34ca425e12ebe3
popensisa <s.islam.like@mail.ru>
2023-02-27 17:42:37 +0600

    remove counter from ui testing

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,4 +1,3 @@
-import { Counter } from 'entities/Counter';
 import { useTranslation } from 'react-i18next';
 
 const MainPage = () => {
@@ -7,7 +6,6 @@ const MainPage = () => {
   return (
     <div>
       {t('Главная страница')}
-      <Counter/>
     </div>
   );
 };
