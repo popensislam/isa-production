da2163f519aeac3874f7d847cdcff23e5f2784b7
popensisa <s.islam.like@mail.ru>
2023-05-03 11:30:00 +0600

    dynamic profile reducer

--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -1,9 +1,10 @@
 import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
 import { loginReducer } from 'features/AuthByUsername';
+import { profileReducer } from 'entities/Profile';
 import type { DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
 import type { Story } from '@storybook/react';
 
-const defaultAsyncReducers: DeepPartial<ReducersMapObject<StateSchema>> = { loginForm: loginReducer };
+const defaultAsyncReducers: DeepPartial<ReducersMapObject<StateSchema>> = { loginForm: loginReducer, profile: profileReducer };
 
 export const StoreDecorator = (
   state: DeepPartial<StateSchema>,
