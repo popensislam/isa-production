18117c8b6cbfa6b7af1046d9966f2a561160b9bb
Женишов Ислам <zhenishov.i@routeamgroup.com>
2023-10-07 17:57:17 +0600

    concurrently, file templates

---
+++ .fttemplates/ComponentTemplate/[FTName].tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './[FTName].module.scss'
+
+interface <FTName | capitalize>Props {
+  className?: string
+}
+
+export const <FTName | capitalize> = (props: <FTName | capitalize>Props) => {
+  return <div></div>
+}
